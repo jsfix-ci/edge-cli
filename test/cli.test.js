e5515e2b6c1c53c10f17ba0f24b274266a756e79
Kamona-WD <ak3849909@gmail.com>
2021-08-03 10:16:29 +0300

    let perttier format test files

--- test/cli.test.js
+++ test/cli.test.js
@@ -3,11 +3,10 @@ import test from 'ava'
 import execa from 'execa'
 import readPkg from 'read-pkg'
 
+const cli = path.resolve('dist/cli.js')
 
-const cli = path.resolve('dist/cli.js');
-
-test('Check version', async t => {
-    const { stdout } = await execa(cli, ['-v']);
-    const { version } = await readPkg(path.dirname(__dirname));
-    t.is(stdout, version);
+test('Check version', async (t) => {
+  const { stdout } = await execa(cli, ['-v'])
+  const { version } = await readPkg(path.dirname(__dirname))
+  t.is(stdout, version)
 })
