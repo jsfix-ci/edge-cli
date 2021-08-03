101dda03deb98ba1df475466acd5f4303248c17f
Kamona-WD <ak3849909@gmail.com>
2021-08-03 11:20:52 +0300

    passing test

--- test/cli.test.js
+++ test/cli.test.js
@@ -7,5 +7,5 @@ const cli = path.resolve('dist/cli.js')
 test('Check version', async () => {
   const { stdout } = await execa(cli, ['-v'])
   const { version } = await readPkg(path.dirname(__dirname))
-  expect(stdout).toEqual(version)
+  expect(stdout).toBe(version)
 })
